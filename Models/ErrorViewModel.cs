3f8304269dfd9259ea6de574be16525dcddee94f
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-06-03 08:27:14 +0900

    プロジェクトを作成

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp_Exercise_Answer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
