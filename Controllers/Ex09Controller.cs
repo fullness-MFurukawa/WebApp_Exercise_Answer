dadf3107458ef1f087fe59ab0f1fd011ac422c56
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-06-13 13:04:23 +0900

    演習-12 PRGパターンとTempDataを利用する

--- Controllers/Ex09Controller.cs
+++ Controllers/Ex09Controller.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 /// <summary>
 /// 演習-09 HTML Formを作成するタグヘルパーを利用する
 /// </summary>
@@ -20,17 +21,42 @@ public class Ex09Controller : Controller
     /// [計算]ボタンクリックアクション
     /// </summary>
     /// <returns></returns>
-    [HttpPost("Result")]
-    public IActionResult Result(Exercise07Form form)
+    [HttpPost("Calc")]
+    public IActionResult Calc(Exercise07Form form)
     {
         // バリデーションチェック
         if (!ModelState.IsValid)
         {
-            return View("Enter",form);
+            // バリデーションエラーの場合入力画面を表示する
+            return View("Enter", form);
         }
-        form.Answer = form.Value1 + form.Value2;
-        return View(form);
+        // Exercise07Formをシリアライズする
+        var json = JsonSerializer.Serialize(form);
+        // TempDataに登録する
+        TempData["Exercise07Form"] = json;
+        // 計算結果を表示するResultへリダイレクトする
+        return RedirectToAction("Result");
     }
+
+        /// <summary>
+        /// 計算処理と結果画面表示アクション
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Result")]
+        public IActionResult Result()
+        {
+            // TempDataからERxercise07Formを取り出す
+            string? json = (string ) TempData["Exercise07Form"]!;
+            if (json == null)
+            {
+                // TempDataにExercise07Formが無い場合、入力画面を表示する
+                return View("Enter");
+            }
+            // 存在する場合はデシリアライズする
+            var form = JsonSerializer.Deserialize<Exercise07Form>(json);
+            form!.Answer = form.Value1 + form.Value2;
+            return View(form);
+        }
 
     /// <summary>
     /// [戻る]ボタンクリックアクション
